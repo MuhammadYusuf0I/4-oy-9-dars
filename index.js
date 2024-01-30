2af3d1ba5d33f88a530bc35e36748b8314b69c78
MuhammadYusuf0I <abduvasiyevmuhammadyusuf28@gmail.com>
2024-01-30 22:42:01 +0500

    vazifa

---
+++ index.js
@@ -0,0 +1,79 @@
+// 1-masal
+
+// let num =[3,6,8,4,2,-1,0,5,-11,-14,-45];
+// function num$(arr) {
+//     let res = arr.sort(function(a,b) {
+//         return a-b
+//     })
+//     res= num[0]
+//     return res
+// }
+// console.log(num$(num));
+
+// 2-masal
+
+// elementlarning toq indekislar yigindisi
+
+// let arr = [2, 4, 6, 8, 5, 7, 3, 9]
+// function number$(array) {
+//     let sum = 0;
+//     for (let i = 0; i < array.length; i++) {
+//       if (i % 2 == 1) {
+//         sum += i;
+//       }
+//     }
+//     return sum
+// }
+ 
+//  console.log(number$(arr))
+
+// 2-masala
+
+// toq elementlar  indekislar yigindisi
+
+// let arr = [2, 4, 6, 8, 5, 7, 3, 9];
+// function number_(array) {
+//     let sum = 0;
+// for (let i = 0; i < array.length; i++) {
+//   if (arr[i] % 2 == 1) {
+//     sum += i;
+//   }
+// }
+// return sum
+// }
+// console.log(number_(arr));
+
+// 3-masala
+
+// let mas=[2,3,5,7,9,1,23,45,37];
+// let tub;
+// for (let i= mas[0]; i<mas.length; i++){
+//     for (let j = 2; j < i; j++) {
+
+//         if (i % j == 0) {
+//           tub = false;
+//           break;
+//         }
+//       }
+
+//       if (tub == true){
+//         console.log(mas[j])
+//       }
+      
+// }
+//  console.log(tub);
+
+// 4-masal
+
+// let mas = [3, 6, 4, 8, 10, 14, 12];
+// function arr(array) {
+//     let cont = 0;
+//     for (let i = 0; i < array.length; i++) {
+//       if (mas[i] % 3 == 0) {
+//         cont++;
+//       }
+//     } 
+//     return cont
+// }
+
+// console.log(arr(mas));
